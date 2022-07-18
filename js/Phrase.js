5fd0688f887c734531147fc78b826d96b44edadb
Zachary Baca <bacazach@icloud.com>
2022-07-18 10:08:11 -0500

    created addPhraseToDisplay method and it works as expected

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,21 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+// Class That Handles Phrases For The Guessing Phrase Game
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    addPhraseToDisplay() {
+        
+        for (let i = 0; i < this.phrase.length; i++) {
+            let listOfLetters = document.querySelector('#phrase ul');
+            let letter = document.createElement('li');
+            let splitPhrase = this.phrase.split('');
+            letter.textContent = splitPhrase[i];
+            listOfLetters.appendChild(letter);
+        }
+    }
+}
+
